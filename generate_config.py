788f51b6fb1d3496199caeaf4419bb02eed4fc8b
S.Naito <na.shota.1139@gmail.com>
2020-04-19 22:03:35 +0900

    speed10

--- generate_config.py
+++ generate_config.py
@@ -49,7 +49,7 @@ def save_file(filename, atoms):
 
 atoms = []
 
-add_ball(atoms, -20, 0.1)
-add_ball(atoms, 20, -0.1)
+add_ball(atoms, -20, 10)
+add_ball(atoms, 20, -10)
 
 save_file("collision.atoms", atoms)
